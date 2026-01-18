6d259939379dd785be14d41baf4c0f704ba998a4
CarlosHSiqueiraB <carlossiqueiraborges@gmail.com>
2026-01-18 19:01:38 -0300

    TiposVariaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        short NumeroCurto = 1;
+        int NumeroNormal = NumeroCurto;
+        short NumeroCurto2 = (short) NumeroNormal;
+
+       final int NUMERO = 5;
+
+        System.out.println(NUMERO);
+    }
+}
