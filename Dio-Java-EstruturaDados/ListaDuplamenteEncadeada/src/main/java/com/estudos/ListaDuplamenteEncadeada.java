6ee1df2177aa0a1b2a8667997ce8b8301802dab1
CarlosHSiqueiraB <carlossiqueiraborges@gmail.com>
2026-05-14 21:14:40 -0300

    Completando o projeto de ListaDuplamenteEncadeada

--- Dio-Java-EstruturaDados/ListaDuplamenteEncadeada/src/main/java/com/estudos/ListaDuplamenteEncadeada.java
+++ Dio-Java-EstruturaDados/ListaDuplamenteEncadeada/src/main/java/com/estudos/ListaDuplamenteEncadeada.java
@@ -31,6 +31,46 @@ public class ListaDuplamenteEncadeada<T> {
         tamanhoLista++;
     }
 
+    public void add(int index, T elemento) {
+        NoDuplo<T> noAux = getNo(index);//Caso o indice seja 0, aqui ja temos o primeiro nó
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(noAux);
+
+        if (novoNo.getNoProximo() != null) {
+            novoNo.setNoPrevio(noAux.getNoPrevio());//Faço o nó prévio virar o novoNo
+            novoNo.getNoProximo().setNoPrevio(novoNo);//E aqui faço o antigo nó prévio virar o novo próximo nó
+        } else {
+            novoNo.setNoPrevio(ultimoNo);
+            ultimoNo = novoNo; //Aqui transformo o ultimo nó da lista no novoNo
+        }
+
+        if (index == 0) {
+            primeiroNo = novoNo;
+        } else {
+            novoNo.getNoPrevio().setNoProximo(novoNo);
+        }
+
+        tamanhoLista++;
+    }
+
+    public void remove(int index) {
+        if (index == 0) {
+            primeiroNo = primeiroNo.getNoProximo();//Agora minha referência de primeiro nó, passa a apontar pro segundo
+            if (primeiroNo != null) {
+                primeiroNo.setNoPrevio(null);
+            }
+        } else {
+            NoDuplo<T> noAux = getNo(index);//Aqui simplesmente recebo o valor do index
+            noAux.getNoPrevio().setNoProximo(noAux.getNoProximo());//Para transformar o nó prévio no próximo nó
+            if (noAux != ultimoNo) {
+                noAux.getNoProximo().setNoPrevio(noAux.getNoPrevio());//Aqui eu tiro a conexão do nó prévio do index escolhido, e passo para o nó prévio dele, tipo escolher o index 3, isso vai fazer o index 2 -> 1 e o 1 -> 0
+            } else {
+                ultimoNo = noAux.getNoPrevio();
+            }
+        }
+        this.tamanhoLista--;
+    }
+
     private NoDuplo<T> getNo(int index) {//Como agora a estrutura é um pouco diferente, também mudamos um pouco a forma do get nó
         NoDuplo<T> noAux = primeiroNo;//Eu começo pelo primeiro nó da lista
         for (int i = 0; (i < index) && (noAux != null); i++) {
@@ -44,5 +84,15 @@ public class ListaDuplamenteEncadeada<T> {
         return tamanhoLista;
     }
 
-
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        NoDuplo<T> noAux = primeiroNo;
+        for (int i = 0; i < size(); i++) {
+            strRetorno += "{No[conteudo=" + noAux + "]} ->";
+            noAux = noAux.getNoProximo();
+        }
+        strRetorno += "null";
+        return strRetorno;
+    }
 }
--- Dio-Java-EstruturaDados/ListaDuplamenteEncadeada/src/main/java/com/estudos/Main.java
+++ Dio-Java-EstruturaDados/ListaDuplamenteEncadeada/src/main/java/com/estudos/Main.java
@@ -1,8 +1,24 @@
 package com.estudos;
 
-
 public class Main {
     public static void main(String[] args) {
+        ListaDuplamenteEncadeada<String> minhaLista_D_Encadeada = new ListaDuplamenteEncadeada<>();
+
+        minhaLista_D_Encadeada.add("C1");
+        minhaLista_D_Encadeada.add("C2");
+        minhaLista_D_Encadeada.add("C3");
+        minhaLista_D_Encadeada.add("C4");
+        minhaLista_D_Encadeada.add("C5");
+        minhaLista_D_Encadeada.add("C6");
+        minhaLista_D_Encadeada.add("C7");
+
+        System.out.println(minhaLista_D_Encadeada);
+
+        minhaLista_D_Encadeada.remove(6);
+        System.out.println(minhaLista_D_Encadeada);
+        minhaLista_D_Encadeada.add(6,"99");
+        System.out.println(minhaLista_D_Encadeada);
+
 
     }
 }
