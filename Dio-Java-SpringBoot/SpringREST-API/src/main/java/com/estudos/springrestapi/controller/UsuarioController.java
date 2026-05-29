e14443db7115c0203a056b32c7e55e9134391d73
CarlosHSiqueiraB <carlossiqueiraborges@gmail.com>
2026-05-29 00:03:53 -0300

    Adicionando o RequestMapping

--- Dio-Java-SpringBoot/SpringREST-API/src/main/java/com/estudos/springrestapi/controller/UsuarioController.java
+++ Dio-Java-SpringBoot/SpringREST-API/src/main/java/com/estudos/springrestapi/controller/UsuarioController.java
@@ -8,26 +8,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users") // Definindo rota default
 public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
-    @GetMapping("/users") // Sempre que formos usar recursos get diferentes, precisamos dizer qual operação deve ser realizada, diferenciando pela chaves, usando mapeamento
+    @GetMapping // Sempre que formos usar recursos get diferentes, precisamos dizer qual operação deve ser realizada, diferenciando pela chaves, usando mapeamento
     public List<Usuario> getUsers(){
         return repository.listAll();
     }
 
-    @GetMapping("users/{login}")
+    @GetMapping("/{login}")
     public Usuario findOne(@PathVariable("login") String login){ // O pathVariable é quem define o valor passado que é no caso o {login}
         return repository.findByLogin(login);
     }
-    @DeleteMapping("users/{id}") // Usar PostMan
+    @DeleteMapping("/{id}") // Usar PostMan
     public String delete(@PathVariable("id") Integer id){
         repository.remove(id);
 
         return "Usuário removido com sucesso";
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public String postUser(@RequestBody Usuario usuario){
         repository.save(usuario);
         return "Usuário Cadastrado com Sucesso!";
