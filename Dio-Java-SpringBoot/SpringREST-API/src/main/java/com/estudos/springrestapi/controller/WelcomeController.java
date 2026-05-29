91665107a5de104de3d40fe886f04368eb973e02
CarlosHSiqueiraB <carlossiqueiraborges@gmail.com>
2026-05-28 22:32:56 -0300

    Minha primeira API SPRINGBOOT

---
+++ Dio-Java-SpringBoot/SpringREST-API/src/main/java/com/estudos/springrestapi/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.estudos.springrestapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Para denominar uma classe como componente REST, preciso chamala de REST CONTROLLER
+public class WelcomeController {
+    @GetMapping // Aqui denomino como algo que vai retornar um recurso HTTP, então chamo de get
+    public String welcome(){
+        return "Welcome to my Spring Boot Web API";
+    }
+}
